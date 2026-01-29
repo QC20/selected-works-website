c0b6882ef72c0fcbfc8bfa784e372dec2ed2eac6
QC20 <xgv866@alumni.ku.dk>
2026-01-30 00:28:04 +0100

    update

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false, // Keep TypeScript errors
+  },
+}
+
+module.exports = nextConfig
